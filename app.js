ee5f44446b72fbc4861c98c88c1b091b2fbf5e52
Abdulrahman A <abodianoful@gmail.com>
2022-06-28 20:46:20 +0300

    housekeeping

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const express=require("express");
 const todoRouters=require("./routes/todo")
 const PORT=3000
-const log=console.log
 
 let app=express()
 
 app.set("view engine","ejs");
 app.use('/public/', express.static('./public'));
-//response as Json
+
 app.use(express.json()); 
 
-//Parse x-www-form-urlencoded request into req.body
-app.use(express.urlencoded({ extended: true }));     
-
-
-//handling routers 
 todoRouters(app)
 
 app.listen(PORT, function(err){
--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,5 @@
 const mongoose=require("mongoose");
 let dbURL = "mongodb://localhost:27017/todo";
-
-
-module.exports=function(app){
-
 mongoose.connect(dbURL);
 
 let todoSchema= new mongoose.Schema(
@@ -15,48 +11,40 @@ let todoSchema= new mongoose.Schema(
 
 let todoModel=mongoose.model("todo",todoSchema)
 
+
+module.exports=function(app){
 // 
+const log=console.log
 
 app.get("/",(req,res)=>{
-    console.log(req.method)
+    log(req.method)
     todoModel.find({},function(err,data){
         if (err) throw err;
-        console.log(data.length+ "0 ---"+data +"-----" +typeof(data) +" ---- "+Array.isArray(data));
+        log(data.length+ "0 ---"+data +"-----" +typeof(data) +" ---- "+Array.isArray(data));
         res.render("index",{tasks:data})
     })
 });
 
 app.post("/:task",(req,res)=>{
     const userTask = req.body
-
-    console.log(req.method+" method "+Object.values(req.body))
-    console.log(JSON.stringify(userTask),"=====",req.body.task)
-    console.log((res))
+    log(req.method+" method "+Object.values(userTask))
+    log(JSON.stringify(userTask),"=====",req.body.task)
 
     todoModel.create(userTask, function (err, post) {
         if (err) return next(err);
-        console.log('added'+ userTask + "response")
+        log('added'+ userTask + "response")
         res.json(post);
-        
-
       });
-    
-
-  
 });
 
 app.delete("/:id",(req,res)=>{
-    console.log(req.method, " ___ ************************** "+req.params.id+"\n\n\n")
+    log(req.method, " ___ ************************** "+req.params.id+"\n\n\n")
     todoModel.findOneAndDelete({_id:req.params.id }, function (err, task) {
         if (err) throw err
-            console.log("id got completed: ", task);
+            log("id got completed: ", task);
     });
 
     
-});
-
-app.put("/",(req,res)=>{
-    res.send(`<h1>${res.method} TODO<h1>`)
 });
 
 }
